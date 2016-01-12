17b2513666f2eecbe7b4161a61bb0092246fda55
FloJ12 <f.jandl91@gmail.com>
2016-01-12 18:59:05 +0100

    Cleaning up for agreed project structure

--- version2/AllThoseTerritories/src/AllThoseTerritories.java
+++ version2/AllThoseTerritories/src/AllThoseTerritories.java
@@ -2,93 +2,99 @@
  * Created by nam on 08.01.16.
  */
 public class AllThoseTerritories {
+    Player user = new Player("User", true);
+    Player ki = new Player("KI", false);
+    Territory[] territories = {}; // TODO: Read Territories from file
+    // TODO: Make methods static
 
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+    }
 
-
-    public static boolean playerTurn(boolean startPhase) {
-        if (startPhase) {
-            // acquire an uncontested territory
-            return false;
+    public void phaseOccupy() {
+        while(!allOccupied()) {
+            user.occupy();
+            ki.occupy();
         }
+    }
 
+    public void phaseConquer() {
+        user.attackAndMove();
+        ki.attackAndMove();
+    }
 
-        // normal turn
-        int reinforcements = checkReinforcements(Possession.ofPlayer);
-        boolean passTurn = false;
-        boolean winCon   = false;
-
-        while (!passTurn && !winCon) {
-            if (reinforcements > 0) {
-                // deploy reinforcements
-            } else {
-                // attack & move
-                winCon = checkWinCon(Possession.ofPlayer);
+    public boolean allOccupied() {
+        for (Territory t : territories) {
+            if (t.owned_by == null) {
+                return false;
             }
         }
-        return winCon;
-    }
-
-    public static boolean opponentTurn(boolean startPhase) {
-        if (startPhase) {
-            // acquire an uncontested territory
-            return false;
-        }
-
-
-        // normal turn
-        int reinforcements = checkReinforcements(Possession.ofOpponent);
-        boolean passTurn = false;
-        boolean winCon   = false;
-
-        while (!passTurn && !winCon) {
-            if (reinforcements > 0) {
-                // deploy reinforcements
-            } else {
-                // attack & move
-                winCon = checkWinCon(Possession.ofOpponent);
-            }
-        }
-        return winCon;
-    }
-
-    public static int checkReinforcements(Possession whichPlayer) {
-        return 0;
-    }
-
-    public static boolean checkWinCon(Possession whichPlayer) {
-        return false;
+        return true;
     }
 }
 
-
-
 class Continent {
     String c_name;
-    int reinforce;
     Territory[] territories;
 
-    Continent(String c_name, int reinforce, int tNum) {
+    Continent(String c_name, int tNum) {
         this.c_name = c_name;
-        this.reinforce = reinforce;
         this.territories = new Territory[tNum];
     }
 }
 
 class Territory {
     String t_name;
-    Possession state;
-    int deploymentStrength;
+    Player owned_by;
+    int armyStrength;
     Territory[] adjacency;
 
     Territory(String t_name, int edgesNum) {
         this.t_name = t_name;
-        this.state = Possession.noMansLand;
-        this.deploymentStrength = 0;
+        this.owned_by = null;
+        this.armyStrength = 0;
         this.adjacency = new Territory[edgesNum];
     }
 }
 
-enum Possession {ofPlayer, ofOpponent, noMansLand}
+class Player {
+    int availableReinforcements = 0;
+    String name;
+    boolean isHuman;
+    public Player(String name, boolean isHuman) {
+        this.name = name;
+        this.isHuman = isHuman;
+    }
+
+    public void occupy() {
+        // TODO: acquire an uncontested territory
+    }
+
+    public void deployReinforcements() {
+        availableReinforcements = checkCountOfReinforcements();
+        if (availableReinforcements > 0) {
+            // TODO: deploy reinforcements
+        }
+    }
+
+    public void attackAndMove() {
+        boolean turnCompleted = false;
+        boolean gameWon   = false;
+
+        while (!turnCompleted && !gameWon) {
+            gameWon = checkIfGameWon();
+            // TODO: attacking and moving of armies
+        }
+    }
+
+    public int checkCountOfReinforcements() {
+        // TODO: count territories owned by this player and calculate resulting available reinforcements
+        return 0;
+    }
+
+    public boolean checkIfGameWon() {
+        // TODO: check if all territories are owned by this player
+        return false;
+    }
+}
 
 
