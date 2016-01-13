6cfff0472986d39a1bab5b3db8c836b9a13650bb
FloJ12 <f.jandl91@gmail.com>
2016-01-13 09:44:21 +0100

    Cleaning up project structure
    No folder version2 any more, removed unnecessary hierarchy layers

--- AllThoseTerritories/src/AllThoseTerritories.java
+++ AllThoseTerritories/src/AllThoseTerritories.java
@@ -3,50 +3,117 @@
  */
 public class AllThoseTerritories {
 
+    private Territory[] territories = {};
+    private Player[] humanPlayers;
+    private Player[] kiPlayers;
+
+    public AllThoseTerritories(Player[] humanPlayers, Player[] kiPlayers) {
+        this.territories = null; // TODO: Read Territories from file
+        this.humanPlayers = humanPlayers;
+        this.kiPlayers = kiPlayers;
+    }
+
+    public void phaseOccupy() {
+        while(!allOccupied()) {
+            for(Player user : humanPlayers) {
+                user.occupy();
+            }
+            for(Player ki : kiPlayers) {
+                ki.occupy();
+            }
+        }
+    }
+
+    public void phaseConquer() {
+        for(Player user : humanPlayers) {
+            user.attackAndMove();
+        }
+        for(Player ki : kiPlayers) {
+            ki.attackAndMove();
+        }
+    }
+
+    private boolean allOccupied() {
+        for (Territory t : territories) {
+            if (t.owned_by == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        startGame();
-    }
-
-
-
-
-    public static void startGame() {
-        setupGame();
-    }
-
-    public static void setupGame() {
-
+        Player user = new Player("User", true);
+        Player ki = new Player("KI", false);
+        AllThoseTerritories game = new AllThoseTerritories(new Player[] {user}, new Player[] {ki});
+        game.phaseOccupy();
+        game.phaseConquer();
     }
 }
 
-
-
 class Continent {
     String c_name;
-    int reinforce;
     Territory[] territories;
 
-    Continent(String c_name, int reinforce, int tNum) {
+    public Continent(String c_name, int tNum) {
         this.c_name = c_name;
-        this.reinforce = reinforce;
         this.territories = new Territory[tNum];
     }
 }
 
 class Territory {
     String t_name;
-    Possession state;
-    int deploymentStrength;
+    Player owned_by;
+    int armyStrength;
     Territory[] adjacency;
 
-    Territory(String t_name, int edgesNum) {
+    public Territory(String t_name, int edgesNum) {
         this.t_name = t_name;
-        this.state = Possession.noMansLand;
-        this.deploymentStrength = 0;
+        this.owned_by = null;
+        this.armyStrength = 0;
         this.adjacency = new Territory[edgesNum];
     }
 }
 
-enum Possession {ofPlayer, ofOpponent, noMansLand}
+class Player {
+    int availableReinforcements = 0;
+    String name;
+    boolean isHuman;
+    public Player(String name, boolean isHuman) {
+        this.name = name;
+        this.isHuman = isHuman;
+    }
+
+    public void occupy() {
+        // TODO: acquire an uncontested territory
+    }
+
+    public void attackAndMove() {
+        boolean turnCompleted = false;
+        boolean gameWon   = false;
+
+        while (!turnCompleted && !gameWon) {
+            gameWon = checkIfGameWon();
+            // TODO: attacking and moving of armies
+        }
+    }
+
+    private void deployReinforcements() {
+        availableReinforcements = checkCountOfReinforcements();
+        if (availableReinforcements > 0) {
+            // TODO: deploy reinforcements
+        }
+    }
+
+    private int checkCountOfReinforcements() {
+        // TODO: count territories owned by this player and calculate resulting available reinforcements
+        return 0;
+    }
+
+    private boolean checkIfGameWon() {
+        // TODO: check if all territories are owned by this player
+        return false;
+    }
+}
 
 
