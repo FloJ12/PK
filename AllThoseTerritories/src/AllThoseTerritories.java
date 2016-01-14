aece82bb3771ac70e1683bd07b461fb1c15219cb
FloJ12 <f.jandl91@gmail.com>
2016-01-14 11:35:56 +0100

    Neighbors of territories implemented

--- AllThoseTerritories/src/AllThoseTerritories.java
+++ AllThoseTerritories/src/AllThoseTerritories.java
@@ -7,11 +7,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class AllThoseTerritories {
 
-    private Territory[] territories = {};
+    private Map<String, Territory> territories;
     private Player[] humanPlayers;
     private Player[] kiPlayers;
 
@@ -21,50 +20,50 @@ public class AllThoseTerritories {
         this.kiPlayers = kiPlayers;
     }
 
-    public Territory[] readTerritories(String pathToMap) {
+    private Map<String, Territory> readTerritories(String pathToMap) {
         try {
             // Two buffered reader exceptions like the examples in the lecture
             BufferedReader in = null;
             try {
-                in = new BufferedReader(new FileReader(pathToMap));
-
-                String line;
-                int countTerritories = 0;
-
-                // Find out count of Territories
-                for(int i = 0; (line = in.readLine()) != null; i++){
-                    String[] parts = line.split(" ");
-                    if(parts[0].equals("patch-of")) {
-                        countTerritories++;
-                    }
-                }
-                in.close();
-                Territory[] readTerritories = new Territory[countTerritories];
-                // Ensure that readTerritories is filled up one index after anoter
-                int tIndex = 0;
+                Map<String, Territory> territoryMap = new HashMap<>();
 
                 // Fill array with Territories
                 in = new BufferedReader(new FileReader(pathToMap));
+                String line;
                 for(int i = 0; (line = in.readLine()) != null; i++){
                     String[] parts = line.split(" ");
+                    String territoryName = getTerritoryName(parts);
                     if(parts[0].equals("patch-of")) {
-                        String territoryName = parts[1];
+                        territoryMap.put(territoryName, new Territory(territoryName));
+                    }
+                    if(parts[0].equals("neighbors-of")) {
+                        // Get place of first neighbor in file
+                        int firstNeigborIndex = getFirstNeighborIndex(parts);
 
-                        // find Territory name (can be 1, 2 or 3 Words)
-                        int firstCoordinateIndex = 2;
-                        for(; !parts[firstCoordinateIndex].matches("[0-9]+");
-                            firstCoordinateIndex++) {
-
-                            // For every item that is not a coordinate concatenate the items
-                            // to get territoryName
-                            territoryName = territoryName + " " + parts[firstCoordinateIndex];
-
+                        // Build a String containing only neighbors seperated by -
+                        // i.e. Alaska-Great Britain-North Western Territory
+                        StringBuilder builder = new StringBuilder();
+                        for (int j = firstNeigborIndex; j < parts.length; j++) {
+                            // Add space only for Territories, not before and after separator - and :
+                            if(!parts[j-1].equals("-") && !parts[j-1].equals(":") && !parts[j].equals("-")) {
+                                builder.append(" " + parts[j]);
+                            } else {
+                                builder.append(parts[j]);
+                            }
                         }
-                        readTerritories[tIndex] = new Territory(territoryName);
-                        tIndex++;
+                        String neighborsString = builder.toString();
+
+                        // Split String to get Array containing only neighbors as strings
+                        String[] neighborsArray = neighborsString.split("-");
+
+                        // Add every corresponding neighbor Territory object to the current territory
+                        for (String neighborString : neighborsArray) {
+                            Territory neighbor = territoryMap.get(neighborString);
+                            territoryMap.get(territoryName).addNeighbor(neighbor);
+                        }
                     }
                 }
-                return readTerritories;
+                return territoryMap;
             } finally {
                 if (in != null) {
                     in.close();
@@ -75,6 +74,33 @@ public class AllThoseTerritories {
             System.err.println("I/O Error: " + ex.getMessage());
         }
         return null;
+    }
+
+    // Returns first index containing neighbors for a "neighbor-of" line
+    // in an array resulting from a split line (separator " ")
+    private int getFirstNeighborIndex(String[] parts) {
+        int firstNeighborIndex = 2;
+        for(; !parts[firstNeighborIndex].equals(":");
+            firstNeighborIndex++) {}
+        return firstNeighborIndex + 1;
+    }
+
+    // Returns territory name from an array resulting
+    // from a split line (separator " ")
+    private String getTerritoryName(String[] parts) {
+        String territoryName = parts[1];
+
+        // find Territory name (can be 1, 2 or 3 Words)
+        int firstCoordinateIndex = 2;
+        for(; !parts[firstCoordinateIndex].matches("[0-9]+") && !parts[firstCoordinateIndex].equals(":");
+            firstCoordinateIndex++) {
+
+            // For every item that is not a coordinate concatenate the items
+            // to get territoryName
+            territoryName = territoryName + " " + parts[firstCoordinateIndex];
+
+        }
+        return territoryName;
     }
 
     public void phaseOccupy() {
@@ -92,7 +118,10 @@ public class AllThoseTerritories {
     public void phaseConquer() {
         // As long as the game is not won, this phase continues
         // Parameter for isWon(Player player) can be anybody who owns a territory
-        while(!isWon(territories[0].owned_by)) {
+        // Get any entry from the Map (http://stackoverflow.com/questions/1509391/how-to-get-the-one-entry-from-hashmap-without-iterating)
+        Map.Entry<String, Territory> random_entry = territories.entrySet().iterator().next();
+        Territory random_territory = random_entry.getValue();
+        while(!isWon(random_territory.owned_by)) {
             for (Player user : humanPlayers) {
                 user.deployReinforcements();
             }
@@ -113,8 +142,10 @@ public class AllThoseTerritories {
     }
 
     private boolean isWon(Player user) {
-        for (Territory t : territories) {
-            if (t.owned_by == user) {
+        // Iterate over whole Map
+        for (Map.Entry<String, Territory> entry : territories.entrySet()) {
+            Territory territory = entry.getValue();
+            if (territory.owned_by == user) {
                 return false;
             }
         }
--- AllThoseTerritories/src/Territory.java
+++ AllThoseTerritories/src/Territory.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Florian on 13.01.2016.
  */
@@ -5,11 +11,15 @@ public class Territory {
     String t_name;
     Player owned_by;
     int armyStrength;
-    Territory[] adjacency; //TODO: Use maps for neighbors
+    private Map<String, Territory> neighbors = new HashMap();
 
     public Territory(String t_name) {
         this.t_name = t_name;
         this.owned_by = null;
         this.armyStrength = 0;
+    }
+
+    public void addNeighbor(Territory neighbor) {
+        neighbors.put(neighbor.t_name, neighbor);
     }
 }
